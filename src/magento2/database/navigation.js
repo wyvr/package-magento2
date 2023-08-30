9911528e45a5d72fa520d05ab838ce76672e5c20
wyvr <wyvr@daviot.at>
2023-08-30 17:43:16 +0200

    :art: format file

--- src/magento2/database/navigation.js
+++ src/magento2/database/navigation.js
@@ -1,18 +1,14 @@
 import { get_cache, set_cache } from '@src/shop/core/cache.mjs';
 
-let db;
-let connection;
-let timer;
-
 export const name = 'magento_navigation';
 
 function get_cache_name(store) {
-return `${name}_${store}`;
+    return `${name}_${store}`;
 }
 
 export function get(store) {
     return get_cache(get_cache_name(store));
 }
 export function set(store, value) {
-    set_cache(get_cache_name(store), value)
+    set_cache(get_cache_name(store), value);
 }
