2e7f79f92520537cf8a92f2fd8188b836e9415de
wyvr <wyvr@daviot.at>
2023-09-03 20:54:46 +0200

    :sparkles: persist generated images

--- src/magento2/core/media_fallback.js
+++ src/magento2/core/media_fallback.js
@@ -2,10 +2,10 @@ import { Config } from '@wyvr/generator/src/utils/config.js';
 import { Logger } from '@wyvr/generator/src/utils/logger.js';
 import { Cwd } from '@wyvr/generator/src/vars/cwd.js';
 import { FOLDER_GEN } from '@wyvr/generator/src/constants/folder.js';
-//import { get } from '@src/shop/core/settings.mjs';
+import { write } from '@wyvr/generator/src/utils/file.js';
 import { readFile } from 'node:fs/promises';
 
-export async function onExec({ returnData, returnJson }) {
+export async function onExec({ request, returnData, returnJson }) {
     const store_name = Config.get('shop.default_store');
     const stores = Config.get('shop.stores');
     const store_id = stores[store_name];
@@ -19,11 +19,15 @@ export async function onExec({ returnData, returnJson }) {
         /**
          * @TODO
          * - process media based on the config
-         * - persist the generated image
          * - create lsit of errored images
          * - create cronjob which removes the list and the images in it to force regeneration of errored images
          */
         const content = await readFile(image_path);
+        // persist the generated image
+        const path = Cwd.get(request.url);
+        console.log(path);
+        write(path, content);
+        
         returnData(content);
     } catch (e) {
         Logger.error('default image error', e.message);
