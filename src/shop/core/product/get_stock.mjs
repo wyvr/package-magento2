caf084047ba28a78e88eea008e66d12b8d4a8ad7
wyvr <wyvr@daviot.at>
2023-09-09 09:00:23 +0200

    :sparkles: use multiple attributes for stock

---
+++ src/shop/core/product/get_stock.mjs
@@ -0,0 +1,24 @@
+import { get_attribute_value } from '../attributes.mjs';
+
+export function get_stock(product) {
+    const stock = {
+        is_in_stock: false,
+        qty: 0,
+    };
+    const stock_attr = get_attribute_value(product, 'stock');
+    if (stock_attr) {
+        Object.entries(stock_attr).forEach(([key, value]) => (stock[key] = value));
+    }
+    const status_attr = get_attribute_value(product, 'quantity_and_stock_status');
+    if (status_attr) {
+        Object.entries(status_attr).forEach(([key, value]) => (stock[key] = value));
+    }
+    if (product?.type_id == 'configurable' && product?.configurable_products) {
+        const child_stocks = product.configurable_products
+            .map((child) => get_attribute_value(child, 'stock'))
+            .filter(Boolean);
+        stock.is_in_stock = child_stocks.find((s) => s.is_in_stock == '1') != null;
+        stock.qty = Math.max(...child_stocks.map((s) => parseFloat(s.qty)));
+    }
+    return stock;
+}
