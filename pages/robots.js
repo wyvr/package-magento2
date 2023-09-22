0948a01f2b59abfbba0146f0e0b69e702cc836b0
wyvr <wyvr@daviot.at>
2023-09-22 10:56:45 +0200

    :bug: fix when not stores are configured for the robots.txt

--- pages/robots.js
+++ pages/robots.js
@@ -1,9 +1,14 @@
 import { load_data } from '@src/shop/core/elasticsearch.mjs';
 import { Config } from '@wyvr/generator/src/utils/config.js';
+import { Logger } from '@wyvr/generator/src/utils/logger.js';
 
 export default async function (page_data) {
     const default_store_name = Config.get('shop.default_store');
     const stores = Config.get('shop.stores');
+    if(!stores || !default_store_name) {
+        Logger.error('robots.txt error, no stores available', default_store_name, stores);
+        return undefined;
+    }
     const default_store = stores[default_store_name];
     const data = await load_data('wyvr_settings', { id: default_store });
     // robots content from magento 2
