662aebe26589efb354f82f54786fd93b771365b1
wyvr <info@wyvr.dev>
2024-06-25 18:03:39 +0200

    feat: sanitize email

--- src/shop/api/customer/login.js
+++ src/shop/api/customer/login.js
@@ -1,13 +1,17 @@
 import { logger, get_error_message } from '@wyvr/generator/universal.js';
 import { jsonOptions, magentoUrl, post } from '@src/shop/api/api.js';
+import { is_valid_email, sanitize_email } from '@src/shop/validator/email.js';
 
 export async function login_customer(store, email, password, isProd) {
     const internal_error = __('shop.internal_error');
     const login_error = __('customer.login_error');
-    // @TODO sanitize email
+    if (!is_valid_email(email)) {
+        logger.warning('magento2 login, invalid email', email);
+        return [login_error, 400, undefined];
+    }
     const payload = {
-        username: email,
-        password: password
+        username: sanitize_email(email),
+        password: password,
     };
 
     const token_url = magentoUrl('/rest/all/V1/integration/customer/token');
@@ -17,11 +21,15 @@ export async function login_customer(store, email, password, isProd) {
         const token_result = await post(
             token_url,
             jsonOptions({
-                body: payload
+                body: payload,
             })
         );
         if (!token_result.ok) {
-            logger.warning('magento2 login, customer token request failed', token_result.status, token_result.statusText);
+            logger.warning(
+                'magento2 login, customer token request failed',
+                token_result.status,
+                token_result.statusText
+            );
             return [login_error, 403, undefined];
         }
         const token = token_result.body;
