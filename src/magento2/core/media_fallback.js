b0e33d9a2b03e7e705b0dfb012a21a481872591d
wyvr <info@wyvr.dev>
2024-07-03 08:02:06 +0200

    fix: avoid persisting invalid file types and return better http status

--- src/magento2/core/media_fallback.js
+++ src/magento2/core/media_fallback.js
@@ -4,6 +4,7 @@ import { Cwd } from '@wyvr/generator/src/vars/cwd.js';
 import { FOLDER_GEN } from '@wyvr/generator/src/constants/folder.js';
 import { write } from '@wyvr/generator/src/utils/file.js';
 import { readFile } from 'node:fs/promises';
+import { extname } from 'node:path';
 
 export async function onExec({ request, returnData, returnJSON, data }) {
     if (!data?.store?.value) {
@@ -20,12 +21,26 @@ export async function onExec({ request, returnData, returnJSON, data }) {
          * - create cronjob which removes the list and the images in it to force regeneration of errored images
          */
         const content = await readFile(image_path);
-        // persist the generated image
-        const path = Cwd.get(request.url);
-        logger.info('fallback media', request.url);
-        write(path, content);
-
-        returnData(content);
+        const ext = extname(request.url);
+        if (
+            [
+                '.jpg',
+                '.jpeg',
+                '.png',
+                '.gif',
+                '.webp',
+                '.avif',
+                '.svg',
+            ].includes(ext)
+        ) {
+            // persist the generated image
+            const path = Cwd.get(request.url);
+            logger.info('fallback media', request.url);
+            write(path, content);
+            return returnData(content);
+        }
+        logger.warning('fallback image, invalid image extension', ext);
+        return returnData(content, 404);
     } catch (e) {
         logger.error('default image error', e.message);
         return returnData('media not found', 404);
