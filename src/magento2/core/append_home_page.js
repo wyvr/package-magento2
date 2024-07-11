164eef057e012d4f3802c36c6a0c16e810805966
wyvr <info@wyvr.dev>
2024-07-11 10:47:57 +0200

    chore: format file

--- src/magento2/core/append_home_page.js
+++ src/magento2/core/append_home_page.js
@@ -2,7 +2,11 @@ import { get_page_by_url } from '@src/magento2/core/data.js';
 import { get } from '@src/shop/core/settings.js';
 
 export async function append_home_page(url, store_id, append_to, redirect_fn) {
-    const home_route = await get(store_id, 'web.default.cms_home_page', `magento 2 home route`);
+    const home_route = await get(
+        store_id,
+        'web.default.cms_home_page',
+        'magento 2 home route'
+    );
 
     if (home_route) {
         append_to.magento_home_route = home_route;
@@ -11,25 +15,34 @@ export async function append_home_page(url, store_id, append_to, redirect_fn) {
         if (home_page_result) {
             const new_home = `/${home_page_result.store}/`;
             append_to.redirect_to = new_home;
-            if (typeof redirect_fn == 'function') {
+            if (typeof redirect_fn === 'function') {
                 redirect_fn(new_home);
             }
         }
         // root page or store page
-        if (url == '/' || url.match(new RegExp(`^/(?:${Object.keys(append_to.stores).join('|')})+/$`))) {
+        if (
+            url === '/' ||
+            url.match(
+                new RegExp(
+                    `^/(?:${Object.keys(append_to.stores).join('|')})+/$`
+                )
+            )
+        ) {
             const page_data = await get_page_by_url(store_id, home_route);
 
-            Object.keys(page_data).forEach((key) => {
+            for (const key of Object.keys(page_data)) {
                 append_to[key] = page_data[key];
-            });
+            }
             append_to.avoid_not_found = true;
             append_to.home_page = true;
             if (!append_to._wyvr) {
                 append_to._wyvr = {};
             }
-            (append_to._wyvr.template = [`shop/HomePage`, 'shop/Default'].concat(append_to._wyvr.template || [])),
-                (append_to._wyvr.persist = true),
-                (append_to._wyvr.language = append_to.locale || 'en');
+            append_to._wyvr.template = ['shop/HomePage', 'shop/Default'].concat(
+                append_to._wyvr.template || []
+            );
+            append_to._wyvr.persist = true;
+            append_to._wyvr.language = append_to.locale || 'en';
         }
     }
     return append_to;
